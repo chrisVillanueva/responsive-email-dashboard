19617fa5d785a74c6cf14e488983e004f5188141
chrisVillanueva <devguy@chrisvillanueva.com>
2017-01-18 21:19:40 -0500

    test

---
+++ working_webpack.config.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {exclude: /node_modules/,loader: 'babel'},
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
